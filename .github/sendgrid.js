8468935b0799fb6665853872f4b38c3563444ec2
sivaprasadhub <94046162+sivaprasadhub@users.noreply.github.com>
2025-08-26 11:48:58 +0000

    updatd code

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
+const sgMail = require('@sendgrid/mail');
 
-const filePath = process.argv[2]; // Path to email.txt file passed as argument
+const filePath = process.argv[2];
 const content = fs.readFileSync(filePath, 'utf8').split('\n').filter(Boolean);
 
-const from = (content[0] && content[0].split(': ')[1]) || '';
-const to = (content[1] && content[1].split(': ')[1]) || '';
-const subject = (content[2] && content[2].split(': ')[1]) || '';
+const from = content[0]?.split(': ')[1] || '';
+const to = content[1]?.split(': ')[1] || '';
+const subject = content[2]?.split(': ')[1] || '';
 const body = content.slice(3).join('\n');
 
-const sendgrid = require('@sendgrid/mail');
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msg = {
   to,
@@ -18,11 +18,9 @@ const msg = {
   text: body,
 };
 
-sendgrid
+sgMail
   .send(msg)
-  .then(() => {
-    console.log('Email sent');
-  })
+  .then(() => console.log('Email sent'))
   .catch((error) => {
     console.error('Error sending email:', error);
     process.exit(1);
