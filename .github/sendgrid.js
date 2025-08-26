8694ff5d01aa3fe7679a3f3301f0cac41e47b3c4
sivaprasadhub <94046162+sivaprasadhub@users.noreply.github.com>
2025-08-26 11:19:03 +0000

    updatd code

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -1,16 +1,29 @@
-#!/usr/bin/env node
-'use strict';
 const fs = require('fs');
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const [emailFile] = process.argv.slice(2);
-const content = fs.readFileSync(emailFile, 'utf8').split(/\r?\n/);
-const from = content[0]?.split(': ')[1] || '';
-const to = content[1]?.split(': ')[1] || '';
-const subject = content[2]?.split(': ')[1] || '';
-const body = content.slice(4).join('\n');
+const filePath = process.argv[2]; // Path to email.txt file passed as argument
+const content = fs.readFileSync(filePath, 'utf8').split('\n').filter(Boolean);
 
-sgMail.send({ from, to, subject, text: body })
-  .then(() => console.log('Email sent successfully'))
-  .catch(err => { console.error('Error:', err.toString()); process.exit(1); });
+const from = (content[0] && content[0].split(': ')[1]) || '';
+const to = (content[1] && content[1].split(': ')[1]) || '';
+const subject = (content[2] && content[2].split(': ')[1]) || '';
+const body = content.slice(3).join('\n');
+
+const sendgrid = require('@sendgrid/mail');
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+const msg = {
+  to,
+  from,
+  subject,
+  text: body,
+};
+
+sendgrid
+  .send(msg)
+  .then(() => {
+    console.log('Email sent');
+  })
+  .catch((error) => {
+    console.error('Error sending email:', error);
+    process.exit(1);
+  });
